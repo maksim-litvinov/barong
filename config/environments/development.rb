a4bf737bc30f617f82a4e91a042b39b2beeb6f70
Dmitry Koval <dkoval@heliostech.fr>
2018-03-20 14:42:03 +0200

    Enable logging in workbench dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,4 +57,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  if ENV['WORKBENCH'] == 'true'
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
 end
