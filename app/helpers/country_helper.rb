07ee6eee4ac7159002f780178b880d7f652eec54
Maksim Litvinov <maksim-litvinov@users.noreply.github.com>
2018-05-17 11:43:45 +0300

    Add validation for profile fields Fix document expire validation Add datepicker and country_select for admin panel

--- app/helpers/country_helper.rb
+++ app/helpers/country_helper.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 module CountryHelper
-  def display_country
+  def display_country(country)
+    selected_country = country || params[:country]
+
     @countries = ISO3166::Country.all.sort
     collection_select(:profile, :country, @countries, :alpha3, :name,
-                      { prompt: 'Select country', selected: params[:country] },
+                      { prompt: 'Select country', selected: selected_country },
                       class: 'form-control form-control-lg underlined')
   end
 end
